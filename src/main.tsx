d6ab97b6204bf7601e89feed4b1a84a46e2e08aa
Adrian <adrianmroz82@gmail.com>
2023-10-26 21:22:51 +0200

    initialize vite project with react-query

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {App} from "./App.tsx";
+import "./index.css";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>
+);
