aaa95e6d3f0e5ad9a127d65ea37b3976eb50a8e7
Adrian <adrianmroz82@gmail.com>
2023-10-26 21:39:19 +0200

    add destructured promises

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { SWAPI_BASE_URL } from "./shared";
 
+// note: final version of fetching all characters
 // const fetchAllCharacters = async (page = 1, allCharacters: Character[] = []) => {
 //   const response = await axios.get(`${SWAPI_URL}/people/?page=${page}`);
 
@@ -26,7 +27,17 @@ export const fetchFirstTenCharacters = () => {
 };
 
 export const fetchCharacter = (id: string) => {
-  console.log("id: ", id);
-
   return axios.get(`${SWAPI_BASE_URL}/people/${parseInt(id) + 1}`);
 };
+
+
+// note: destructured version proposal
+// export const fetchFirstTenCharacters = async () => {
+//   const response = await axios.get(`${SWAPI_BASE_URL}/people`);
+//   return response.data.results;
+// };
+
+// export const fetchCharacter = async (id: string) => {
+//   const response = await axios.get(`${SWAPI_BASE_URL}/people/${parseInt(id) + 1}`);
+//   return response.data;
+// };
