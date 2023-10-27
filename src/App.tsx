fa29d63f87ac706cb5e1c68d2346b8dfe120f3de
Adrian <adrianmroz82@gmail.com>
2023-10-27 22:31:27 +0200

    add new detail routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Characters } from "./pages/Characters";
 import { CharacterDetails } from "./pages/CharacterDetails";
 import { ErrorPage } from "./pages/ErrorPage";
 import classes from "./App.module.scss";
+import { VehicleDetails } from "./pages/VehicleDetails";
+import { HomeworldDetails } from "./pages/PlanetDetails";
 
 export function App() {
   return (
@@ -11,8 +13,8 @@ export function App() {
         <Routes>
           <Route path="/" element={<Characters />} />
           <Route path="/character/:id" element={<CharacterDetails />} />
-          {/* <Route path="/vehicles/:id" element={<VehicleDetails />} /> */}
-          {/* <Route path="/planets/:id" element={<HomeworldDetails />} /> */}
+          <Route path="/vehicles/:id" element={<VehicleDetails />} />
+          <Route path="/planets/:id" element={<HomeworldDetails />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Router>
