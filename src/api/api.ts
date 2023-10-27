937c8c60c5a918a1e704c724fe33a692ecd88c42
Adrian <adrianmroz82@gmail.com>
2023-10-27 22:31:13 +0200

    add data queries

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { SWAPI_BASE_URL } from "./shared";
+import { Vehicle } from "../models/vehicle.model";
+import { Homeworld } from "../models/homeworld.model";
 
 // note: final version of fetching all characters
 // const fetchAllCharacters = async (page = 1, allCharacters: Character[] = []) => {
@@ -30,6 +32,15 @@ export const fetchCharacter = (id: string) => {
   return axios.get(`${SWAPI_BASE_URL}/people/${parseInt(id) + 1}`);
 };
 
+// export const fetchConnectedVehicles = (vehicleUrls: string[]) => {
+//   console.log("vehicleUrls", vehicleUrls);
+//   return Promise.all(vehicleUrls.map((url) => axios.get(url)));
+// };
+
+export const fetchConnectedVehicles = async (dataUrls: string[]) => {
+  const responseArray = await Promise.all(dataUrls.map((url) => axios.get(url)));
+  return responseArray.map((response) => response.data) as Vehicle[];
+};
 
 // note: destructured version proposal
 // export const fetchFirstTenCharacters = async () => {
@@ -41,3 +52,18 @@ export const fetchCharacter = (id: string) => {
 //   const response = await axios.get(`${SWAPI_BASE_URL}/people/${parseInt(id) + 1}`);
 //   return response.data;
 // };
+
+export const fetchConnectedHomeworld = async (homeworldUrl: string) => {
+  const response = await axios.get(homeworldUrl);
+  return response.data as Homeworld;
+};
+
+export const fetchVehicle = async (vehicleId: string) => {
+  const response = await axios.get(`${SWAPI_BASE_URL}/vehicles/${parseInt(vehicleId)}`);
+  return response.data as Vehicle;
+};
+
+export const fetchHomeworld = async (homeworldId: string) => {
+  const response = await axios.get(`${SWAPI_BASE_URL}/planets/${parseInt(homeworldId)}`);
+  return response.data as Homeworld;
+};
