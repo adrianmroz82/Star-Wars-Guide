2c3991f5d1ba6c64cb78d9d3fe8b57473e19bc36
Adrian <adrianmroz82@gmail.com>
2023-10-28 09:13:52 +0200

    add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Characters } from "./pages/Characters";
 import { CharacterDetails } from "./pages/CharacterDetails";
 import { ErrorPage } from "./pages/ErrorPage";
-import classes from "./App.module.scss";
 import { VehicleDetails } from "./pages/VehicleDetails";
 import { HomeworldDetails } from "./pages/PlanetDetails";
+import { Vehicles } from "./pages/Vehicles";
+
+import classes from "./App.module.scss";
 
 export function App() {
   return (
@@ -12,7 +14,8 @@ export function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Characters />} />
-          <Route path="/character/:id" element={<CharacterDetails />} />
+          <Route path="/vehicles" element={<Vehicles />} />
+          <Route path="/people/:id" element={<CharacterDetails />} />
           <Route path="/vehicles/:id" element={<VehicleDetails />} />
           <Route path="/planets/:id" element={<HomeworldDetails />} />
           <Route path="*" element={<ErrorPage />} />
