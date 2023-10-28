2450a3b545e39f3dd98d3e7c64b5ffc0a4a6a2cb
Adrian <adrianmroz82@gmail.com>
2023-10-28 09:13:42 +0200

    extract id from both object and objects array

--- src/utils/extractRouteId.ts
+++ src/utils/extractRouteId.ts
@@ -1,25 +1,17 @@
+// TODO : FIX TYPE
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 export function extractRouteId(data: any | any[]) {
-  console.log("data", data);
-
-  // if array data[index]
-
   if (Array.isArray(data)) {
-    // for (const item of data) {
-    //   const parts = item.url.split("/");
-    //   const dataIndex = parts[parts.length - 2];
-    //   console.log("dataIndex", dataIndex);
-    //   return dataIndex;
-    // }
-    for (let i = 0; i < data.length; i++) {
-      const parts = data[i].url.split("/");
-      const dataIndex = parts[parts.length - 2];
-      console.log("dataIndex", dataIndex);
-      return dataIndex;
-    }
+    const ids = data.map((item) => {
+      const parts = item.url.split("/");
+      return parts[parts.length - 2];
+    });
+
+    return ids;
   }
+
   const parts = data.url.split("/");
   const dataIndex = parts[parts.length - 2];
-  console.log("dataIndex", dataIndex);
   return dataIndex;
 }
