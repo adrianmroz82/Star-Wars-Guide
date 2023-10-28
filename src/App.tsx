1b4871fb9e172496880254e77d5d31617027fd0e
Adrian <adrianmroz82@gmail.com>
2023-10-28 09:30:47 +0200

    add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,17 @@ import { HomeworldDetails } from "./pages/PlanetDetails";
 import { Vehicles } from "./pages/Vehicles";
 
 import classes from "./App.module.scss";
+import { LinkedEntities } from "./components/LinkedEntities/LinkedEntities";
 
 export function App() {
   return (
     <div className={classes.container}>
       <Router>
+        <LinkedEntities />
         <Routes>
           <Route path="/" element={<Characters />} />
-          <Route path="/vehicles" element={<Vehicles />} />
           <Route path="/people/:id" element={<CharacterDetails />} />
+          <Route path="/vehicles" element={<Vehicles />} />
           <Route path="/vehicles/:id" element={<VehicleDetails />} />
           <Route path="/planets/:id" element={<HomeworldDetails />} />
           <Route path="*" element={<ErrorPage />} />
