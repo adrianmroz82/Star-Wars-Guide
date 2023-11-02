2e23a614f1ef5b90bbac759d05dede4eee94d36b
Adrian <adrianmroz82@gmail.com>
2023-11-02 22:17:53 +0100

    add paginated call for characters

--- src/api/character.api.ts
+++ src/api/character.api.ts
@@ -2,25 +2,15 @@ import axios from "axios";
 import { Character } from "../models/character.model";
 import { httpService } from "./utils";
 
-const fetchAllCharacters = async (page = 1, allCharacters: Character[] = []): Promise<Character[]> => {
-  const response = await httpService.get(`/people/?page=${page}`);
-
-  if (response.data.results) {
-    allCharacters.push(...response.data.results);
-  }
-
-  if (response.data.next) {
-    const nextPage = page + 1;
-    return fetchAllCharacters(nextPage, allCharacters);
-  } else {
-    allCharacters.sort((a, b) => a.name.localeCompare(b.name));
-    return allCharacters;
-  }
-};
-
-export const fetchCharacters = async () => {
-  const allCharacters = await fetchAllCharacters();
-  return allCharacters;
+export const fetchFirstTenCharacters = async (): Promise<{
+  results: Character[];
+  next: string | null;
+  previous: string | null;
+  count: number;
+}> => {
+  const response = await httpService.get(`/people/?page=1`);
+  const { results, next, previous, count } = response.data;
+  return { results, next, previous, count };
 };
 
 export const fetchCharacter = async (id: string) => {
@@ -32,8 +22,6 @@ export const fetchConnectedCharacters = async (characterUrls: string[]) => {
   const responseArray = await Promise.all(characterUrls.map((url) => axios.get(url)));
   return responseArray.map((response) => response.data) as Character[];
 };
-
-// pagination
 
 export const fetchCharactersByPage = async (page: number) => {
   const response = await httpService.get(`/people/?page=${page}`);
