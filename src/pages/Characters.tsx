d5edae4d1ca7ba3044e7faab4cf57bfb20fab727
Adrian <adrianmroz82@gmail.com>
2023-11-08 23:06:27 +0100

    paginate characters query

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -1,7 +1,14 @@
 import { Character } from "../models/character.model";
 import { GenericPage } from "../components/GenericPage/GenericPage";
-import { fetchCharacters } from "../api/character.api";
+import { fetchCharactersByPage } from "../api/character.api";
+
+interface Props {
+  results: Character[];
+  next: string | null;
+  previous: string | null;
+  count: number;
+}
 
 export function Characters() {
-  return <GenericPage<Character[]> queryKey="characters" fetchFunction={fetchCharacters} path="people" />;
+  return <GenericPage<Props> queryKey="characters" fetchFunction={fetchCharactersByPage} path="people" />;
 }
