8efa5003da8969ecfe8885b5df6001e8c3eb5ea4
Adrian <adrianmroz82@gmail.com>
2023-11-13 23:28:29 +0100

    add caching in pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Entity } from "../../models/entity.model";
 import { fetchCharactersByPage } from "../../api/character.api";
+import { useQuery } from "react-query";
 
-interface PropsX {
+import classes from "./Pagination.module.scss";
+
+interface ResultType {
   results: Entity[];
   next: string | null;
   previous: string | null;
@@ -10,40 +13,41 @@ interface PropsX {
 }
 
 interface Props {
-  results: PropsX;
-  setResults: React.Dispatch<React.SetStateAction<PropsX>>;
-  setIsLoading: boolean;
+  results: ResultType;
+  isLoading: boolean;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  setResults: React.Dispatch<React.SetStateAction<ResultType>>;
 }
 
 export function Pagination({ results, setResults, setIsLoading }: Props) {
-  console.log(results);
-
   const [page, setPage] = useState(1);
-  const [res, setRes] = useState(results.results);
-  // const [totalPages, setTotalPages] = useState(1);
+  const { data, isLoading } = useQuery(["characters", page], () => fetchCharactersByPage(page), {});
+
+  useEffect(() => {
+    if (!isLoading) {
+      setResults(data!);
+    }
+  }, [data, isLoading, setIsLoading, setResults]);
+
   const totalPages = Math.ceil(results.count / results.results.length);
-  console.log(totalPages);
 
   const handlePageChange = async (page: number) => {
-    setIsLoading(true);
     setPage(page);
-    const x = await fetchCharactersByPage(page);
-    setIsLoading(false);
-    setRes(x.results);
-    setResults(x);
   };
 
-  console.log("xp", res);
-
   return (
-    <div>
-      <button onClick={() => handlePageChange(page - 1)} disabled={!results.previous}>
-        Previous
+    <div className={classes.paginationContainer}>
+      <button
+        style={page === 0 ? { display: "none" } : { display: "block" }}
+        className={classes.button}
+        onClick={() => handlePageChange(page - 1)}
+        disabled={!results.previous}>
+        Prev
       </button>
       <span>
         Page {page} of {totalPages}
       </span>
-      <button onClick={() => handlePageChange(page + 1)} disabled={page === totalPages}>
+      <button className={classes.button} onClick={() => handlePageChange(page + 1)} disabled={page === totalPages}>
         Next
       </button>
     </div>
