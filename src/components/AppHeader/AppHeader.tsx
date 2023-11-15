153ad7d9e8c5718da137ac6b4b3087466c942e47
Adrian <adrianmroz82@gmail.com>
2023-11-15 22:08:45 +0100

    add header font

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -2,10 +2,10 @@ import classes from "./AppHeader.module.scss";
 
 export function AppHeader() {
   return (
-    <header className={classes.headerContainer}>
-      <h1 className={classes.header}>Star</h1>
-      <h3>Visual guide</h3>
-      <h1 className={classes.header}>Wars</h1>
-    </header>
+    <div className={classes.headerContainer}>
+      <h1>Star</h1>
+      <h3>A Visual guide</h3>
+      <h1>Wars</h1>
+    </div>
   );
 }
